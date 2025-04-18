86c4f0f2c7af8c5609ddbb4054bc81ce5a9195dc
Yohikerfreites <yohikerf15@gmail.com>
2025-04-18 21:21:33 +0200

    Commit_queries
    Todas las consultas listas

---
+++ IntroBBDD.sql
@@ -0,0 +1,25 @@
+1º
+SELECT flight_id, flight_no, status
+FROM flights
+WHERE status = 'On Time';
+
+2º
+SELECT *
+FROM bookings
+WHERE total_amount >= 1000000;
+
+3º
+SELECT *
+FROM aircrafts_data;
+
+4º
+SELECT flight_id, flight_no
+FROM flights
+WHERE aircraft_code = '733';
+
+5º
+SELECT *
+FROM tickets
+WHERE passenger_name LIKE 'IRINA%';
+
+6º
